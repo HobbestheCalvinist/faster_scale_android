5a7c7c5707d6fdba26d2e2a8b255d369efd90e75
HobbestheCalvinist <joshkava@gmail.com>
2026-05-03 07:23:42 -0400

    Call schedule sorts by date added.

--- app/src/main/java/com/example/myapplication/CallScheduleDao.kt
+++ app/src/main/java/com/example/myapplication/CallScheduleDao.kt
@@ -5,7 +5,23 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CallScheduleDao {
-    @Query("SELECT * FROM call_schedules")
+    @Query("""
+        SELECT * FROM call_schedules 
+        ORDER BY 
+            CASE dayOfWeek 
+                WHEN 'Monday' THEN 1 
+                WHEN 'Tuesday' THEN 2 
+                WHEN 'Wednesday' THEN 3 
+                WHEN 'Thursday' THEN 4 
+                WHEN 'Friday' THEN 5 
+                WHEN 'Saturday' THEN 6 
+                WHEN 'Sunday' THEN 7 
+                ELSE 8 
+            END,
+            SUBSTR(time, 7, 2) ASC, 
+            CASE WHEN SUBSTR(time, 1, 2) = '12' THEN '00' ELSE SUBSTR(time, 1, 2) END ASC, 
+            SUBSTR(time, 4, 2) ASC
+    """)
     fun getAllSchedules(): Flow<List<CallSchedule>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
